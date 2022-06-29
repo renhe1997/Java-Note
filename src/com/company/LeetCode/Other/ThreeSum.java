19c16a1e2cf1fce8f4243ec0c89a6824709e9c48
renhe1997 <1084028573@qq.com>
2022-06-29 15:50:05 +0800

    三数之和双指针做法

--- src/com/company/LeetCode/Other/ThreeSum.java
+++ src/com/company/LeetCode/Other/ThreeSum.java
@@ -11,9 +11,53 @@ import java.util.List;
  */
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] nums = {-2,-1,-1,0,1,3,3,2,6};
-        System.out.println(getThreeSum(nums).toString());
+//        int[] nums = {-2,-1,-1,0,1,3,3,2,6};
+//        int[] nums = {0,0,0,0};
+        int[] nums ={-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums).toString());
     }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List resultList =  new ArrayList();
+        for(int i = 0;i<nums.length-2;i++){
+            if (nums[i]>0){
+                break;
+            }
+            //需要和上一次枚举的数不相同,-1取上次
+            if (i>0&&(nums[i]==nums[i-1])){
+                continue;
+            }
+            int one = -nums[i];
+            int first = i+1;
+            int second = nums.length-1;
+            while(first<second){
+                if((nums[first]+nums[second])<one){
+                    first++;
+                }else if((nums[first]+nums[second])>one){
+                    second--;
+                }else{
+                    while(first<second&&nums[first]==nums[first+1]){
+                        first++;
+                    }
+                    while(first<second&&nums[second]==nums[second-1]){
+                        second--;
+                    }
+                    List intList = new ArrayList();
+                    intList.add(nums[i]);
+                    intList.add(nums[first]);
+                    intList.add(nums[second]);
+                    resultList.add(intList);
+                    // 这里不能结束循环，因为可能存在多个组合使得(nums[first]+nums[second])=one，所以应该挪移指针，继续循环。
+                    first++;second--;
+                    continue;
+                }
+            }
+        }
+
+        return resultList;
+    }
+
     public static List<List> getThreeSum(int[] nums){
         List<List> resultList = new ArrayList();
         //排序
